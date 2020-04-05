ae5f1ecefdff3d568d6000ff20bc82d60b6ef2e0
KarlTaal <44434743+KarlTaal@users.noreply.github.com>
2020-04-05 18:50:53 +0300

    this code took 3200th place in Silver

--- silverToGold.cpp
+++ silverToGold.cpp
@@ -6,6 +6,11 @@
 
 using namespace std;
 
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
 int main() {
     vector <vector<int>> checkPoints;
     bool boostUsed = false;
